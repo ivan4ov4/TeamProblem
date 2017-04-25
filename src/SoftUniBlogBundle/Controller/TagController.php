73083d424e65e005533ea9ff8060dc340e0d0012
DanielVasilev98 <kyn_aguero_10@abv.bg>
2017-04-25 16:20:09 +0300

    functionality added

---
+++ src/SoftUniBlogBundle/Controller/TagController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SoftUniBlogBundle\Controller;
+
+use SoftUniBlogBundle\Entity\Tag;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TagController extends Controller
+{
+    /**
+     * @Route("/tags/{name}", name="articles_by_tag")
+     * @param $name
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function articles($name)
+    {
+        $tag = $this->getDoctrine()->getRepository(Tag::class)
+            ->findOneBy(['name'=>$name]);
+        return $this->render('tags/articles.html.twig', array('tag' => $tag));
+    }
+}
